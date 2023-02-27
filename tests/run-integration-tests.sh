5d0015ca8d1546422c76c59910a68e5da7504244
Kacper Łukawski <lukawski.kacper@gmail.com>
2023-02-27 13:03:27 +0100

    Add run script for integration tests

---
+++ tests/run-integration-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+
+function stop_docker()
+{
+  echo "stopping qdrant_test"
+  docker stop qdrant_test
+}
+
+# Ensure current path is project root
+cd "$(dirname "$0")/../"
+
+QDRANT_VERSION=${QDRANT_VERSION:-'v1.0.0'}
+
+QDRANT_HOST='localhost:6333'
+
+docker run -d --rm \
+           --network=host \
+           -e QDRANT__SERVICE__GRPC_PORT="6334" \
+           --name qdrant_test qdrant/qdrant:${QDRANT_VERSION}
+
+trap stop_docker SIGINT
+trap stop_docker ERR
+
+until $(curl --output /dev/null --silent --get --fail http://$QDRANT_HOST/collections); do
+  printf 'waiting for server to start...'
+  sleep 5
+done
+
+pytest ./tests/integration
+
+echo "Ok, that is enough"
+
+stop_docker
