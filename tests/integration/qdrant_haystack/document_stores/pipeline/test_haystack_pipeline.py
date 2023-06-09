f1a74c1c447fbaa962d9b45bc8623b19bb8357b0
Kacper Łukawski <lukawski.kacper@gmail.com>
2023-06-09 13:27:08 +0200

    Fix loading the QdrantDocumentSore from pipeline

--- tests/integration/qdrant_haystack/document_stores/pipeline/test_haystack_pipeline.py
+++ tests/integration/qdrant_haystack/document_stores/pipeline/test_haystack_pipeline.py
@@ -1,66 +1,44 @@
-from pathlib import Path
-from typing import Dict, Optional
-
+import yaml
 from haystack import Pipeline
-from haystack.nodes.base import BaseComponent
-from haystack.pipelines.config import (
-    get_component_definitions,
-    get_pipeline_definition,
-    read_pipeline_config_from_yaml,
-)
 
-BASE_DIR = Path(__file__).parent
+from qdrant_haystack import QdrantDocumentStore  # noqa
 
 
-class HaystackPipeline(Pipeline):
-    @classmethod
-    def load_from_config(
-        cls,
-        pipeline_config: Dict,
-        pipeline_name: Optional[str] = None,
-        overwrite_with_env_variables: bool = True,
-        strict_version_check: bool = False,
-    ):
-        pipeline = cls()
+def test_qdrant_document_store_works_in_pipelines():
+    pipeline_yaml_string = """
+    version: ignore
 
-        pipeline_definition = get_pipeline_definition(
-            pipeline_config=pipeline_config, pipeline_name=pipeline_name
-        )
-        component_definitions = get_component_definitions(
-            pipeline_config=pipeline_config,
-            overwrite_with_env_variables=overwrite_with_env_variables,
-        )
-        components: Dict[str, BaseComponent] = {}
-        for node_config in pipeline_definition["nodes"]:
-            component = cls._load_or_get_component(
-                name=node_config["name"],
-                definitions=component_definitions,
-                components=components,
-            )
-            pipeline.add_node(
-                component=component,
-                name=node_config["name"],
-                inputs=node_config["inputs"],
-            )
+    components:    # define all the building-blocks for Pipeline
+      - name: DocumentStore
+        type: QdrantDocumentStore
+        params:
+          location: ':memory:'
+          index: 'Document'
+          embedding_dim: 512
+          recreate_index: True
+      - name: Retriever
+        type: BM25Retriever
+        params:
+          document_store: DocumentStore
+          top_k: 5
+      - name: Reader
+        type: FARMReader
+        params:
+          model_name_or_path: deepset/roberta-base-squad2
+          context_window_size: 500
+          return_no_answer: true
 
-        pipeline.update_config_hash()
-        return pipeline
+    pipelines:
+      - name: query    # a sample extractive-qa Pipeline
+        nodes:
+          - name: Retriever
+            inputs: [Query]
+          - name: Reader
+            inputs: [Retriever]
+    """
 
+    pipeline = Pipeline.load_from_config(
+        yaml.safe_load(pipeline_yaml_string), pipeline_name="query"
+    )
 
-def test_query_pipeline_has_access_to_qdrant():
-    config = read_pipeline_config_from_yaml(BASE_DIR / "pipeline.yaml")
-
-    pipelines = {}
-
-    for p in config["pipelines"]:
-        pipelines[p["name"]] = HaystackPipeline().load_from_yaml(
-            path=BASE_DIR / "pipeline.yaml", pipeline_name=p["name"]
-        )
-
-    # pipelines["indexing_pipeline"].graph.nodes
-    # pipelines["query_pipeline"].graph.nodes
-
-    pipelines["indexing_pipeline"].run(file_paths=BASE_DIR / "documents.json")
-    results = pipelines["query_pipeline"].run("what should i do if i lose my card?")
-    print(results)
-    assert False
+    assert pipeline is not None
