88b2d6e28af71dc1350cb23257adb7b901117e81
George <panchuk.george@outlook.com>
2023-09-12 13:28:03 +0400

    fix: fix passing metadata (#31)
    * fix: fix passing metadata, update .gitignore
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: fix pattern in gitignore
    
    * Restrict tiktoken to <=0.4.0
    
    * Update poetry.lock
    
    * Add test for passing metadata
    
    * Restrict qdrant-client to <1.5.0, adapt tests and metadata passing
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Kacper Łukawski <lukawski.kacper@gmail.com>

---
+++ tests/unit/qdrant_haystack/document_stores/test_init.py
@@ -0,0 +1,34 @@
+import tempfile
+
+import pytest
+from qdrant_client.http.exceptions import ResponseHandlingException
+
+from qdrant_haystack import QdrantDocumentStore
+
+
+def test_passing_metadata_propagates_as_rest_headers():
+    """
+    Test that passing metadata propagates as rest headers. It simply checks if the
+    exception thrown by QdrantClient is ResponseHandlingException, as it means that
+    the initialization went fine.
+    :return:
+    """
+    with pytest.raises(ResponseHandlingException):
+        QdrantDocumentStore("http://localhost:6333", metadata={"foo": "bar"})
+
+
+def test_delete_qdrant_doc_store_does_not_throw_exceptions():
+    """
+    Test that deleting QdrantDocumentStore does not throw TypeError exception
+    https://github.com/qdrant/qdrant-haystack/issues/29#issuecomment-1708277697
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+        document_store = QdrantDocumentStore(
+            path=tmpdir,
+            index="Document",
+            embedding_dim=100,
+            recreate_index=True,
+            hnsw_config={"m": 16, "ef_construct": 64},
+        )
+        del document_store
+        assert True
